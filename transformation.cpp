461356a37349deb22da83975ef942f9edb7eb29b
Yogi-official <35768778+Yogi-official@users.noreply.github.com>
2018-03-15 22:45:18 +0530

    tranformation completed 15/3/2018

--- transformation.cpp
+++ transformation.cpp
@@ -6,20 +6,70 @@ Use operator overloading.*/
 
 #include <iostream>
 #include <graphics.h>
+#include <math.h>
 
 using namespace std;
 
 class Transformation{
-	int tx,ty,sx,sy,n;
+	int n,xmid,ymid;	
 
 public:
+		void setparameter(int midx,int midy){
+			xmid=midx;
+			ymid=midy;
+		}
 		void getdata(int arr[][3],int);
-		void draw_image(int a[][3],int,int);
-		void scaling();
-		void transformations();
-		void rotation();
+		void draw_image(int a[][3],bool);
+		void scaling(int a[][3],int,int);
+		void translation(int a[][3],int,int);
+		void rotation(int a[][3],float);
 
 };
+
+void Transformation::rotation(int a[][3],float ang){
+	float angle=3.14*ang/180;
+	float rotate[2][2]={cos(angle),sin(angle),-sin(angle),cos(angle)};
+
+	int result[n][3];
+
+	for(int i=0;i<n;i++)
+		result[i][0]=result[i][1]=result[i][2]=0;
+
+	for(int i=0;i<n;i++)
+		for(int j=0;j<2;j++)
+			for(int k=0;k<2;k++)
+				result[i][j]=result[i][j]+rotate[k][j]*a[i][k];
+
+	draw_image(result,0);
+
+}
+
+void Transformation::translation(int a[][3],int tx,int ty){
+	for(int i=0;i<n;i++){
+		a[i][0]=a[i][0]+tx;
+		a[i][1]=a[i][1]+ty;
+	}
+
+	draw_image(a,0);
+}
+
+void Transformation::scaling(int a[][3],int sx,int sy){
+	int scaled[2][2]={sx,0,0,sy};
+	int result[n][3];
+
+	for(int i=0;i<n;i++)
+		result[i][0]=result[i][1]=result[i][2]=0;
+
+	
+
+	for(int i=0;i<n;i++)
+		for(int j=0;j<2;j++)
+			for(int k=0;k<2;k++)
+				result[i][j]=result[i][j]+scaled[k][j]*a[i][k];
+
+	draw_image(result,0);		
+
+}
 
 void Transformation::getdata(int arr[][3],int nn){
 	n=nn;	
@@ -32,15 +82,22 @@ void Transformation::getdata(int arr[][3],int nn){
 		cin>>arr[i][1];
 		arr[i][2]=1;
 	}
-
 }
 
-void Transformation::draw_image(int arr[][3],int xmid,int ymid){
+void Transformation::draw_image(int arr[][3],bool choice){
 	
+	if(choice==0){
 	for(int i=0;i<n-1;i++)
 		line(xmid+arr[i][0],ymid-arr[i][1],xmid+arr[i+1][0],ymid-arr[i+1][1]);
 		
 	line(xmid+arr[n-1][0],ymid-arr[n-1][1],xmid+arr[0][0],ymid-arr[0][1]);
+	}
+	else{
+		for(int i=0;i<n-1;i++)
+		line(arr[i][0],ymid-arr[i][1],arr[i+1][0],ymid-arr[i+1][1]);
+		
+	line(arr[n-1][0],ymid-arr[n-1][1],arr[0][0],ymid-arr[0][1]);	
+	}
 }
 
 int main(){
@@ -51,16 +108,84 @@ int main(){
 	int arr[n][3];
 	t.getdata(arr,n);
 
-	int gd=DETECT,gm=VGAMAX,xmid,ymid;
-	initgraph(&gd,&gm,NULL);
+
+	int choice;
+	cout<<"\n\n1.scaling \n2.translation\n3.rotation\n4.exit\n\nenter choice : ";
+	cin>>choice;
+
+
+	int gd=DETECT,gm=VGAMAX,xmid,ymid,sx,sy,tx,ty; float angle;
+
+		switch(choice){
+			case 1:{
+				cout<<"\n\n enter scaling factor : ";
+				cout<<"\n\tsx : ";
+				cin>>sx;
+				cout<<"\t\tsy : ";
+				cin>>sy;
+
+				initgraph(&gd,&gm,NULL);
+				xmid=getmaxx()/2;
+				ymid=getmaxy()/2;
+
+				t.setparameter(xmid,ymid);
+				t.draw_image(arr,1);
+				t.scaling(arr,sx,sy);
+	
+				getch();
+				closegraph();
+				break;
+			}
+
+		case 2:{
+			cout<<"\n\n enter translating factor : ";
+				cout<<"\n\ttx : ";
+				cin>>tx;
+				cout<<"\t\tty : ";
+				cin>>ty;
+
+			initgraph(&gd,&gm,NULL);
 
 		xmid=getmaxx()/2;
 		ymid=getmaxy()/2;
 
-		t.draw_image(arr,xmid,ymid);
-		getch();
+		t.setparameter(xmid,ymid);
 
-		closegraph();
+		t.draw_image(arr,0);
+				
+				t.translation(arr,tx,ty);
+				getch();
+				closegraph();
+				break;
+			}
+
+			case 3:
+					{
+						cout<<"\n\nenter angle of rotation : ";
+						cin>>angle;
+
+						initgraph(&gd,&gm,NULL);
+
+						xmid=getmaxx()/2;
+						ymid=getmaxy()/2;
+
+						t.setparameter(xmid,ymid);
+
+						t.draw_image(arr,1);
+						t.rotation(arr,angle);
+						getch();
+						closegraph();
+						break;
+					}
+
+			case 4:
+					exit(0);
+
+			default :
+				cout<<"\n\ninvalid choice";
+				break;
+			}	
+		
 
 	return 0;
 }
